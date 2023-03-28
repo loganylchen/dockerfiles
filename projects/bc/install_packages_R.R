ed2721da064c0842bb86a7522bb33047950fefea
loganylchen <yuelong.chen.btr@gmail.com>
2023-03-28 14:51:45 +0800

    "languageserver"

--- projects/bc/install_packages_R.R
+++ projects/bc/install_packages_R.R
@@ -1,4 +1,4 @@
-install_packages <- c("BiocManager", "devtools", "dplyr", "ggplot2", "circlize", "gridExtra", "ggtern", "scatterplot3d", "Seurat", "statmod", "ggalt","IRkernel")
+install_packages <- c("BiocManager","languageserver", "devtools", "dplyr", "ggplot2", "circlize", "gridExtra", "ggtern", "scatterplot3d", "Seurat", "statmod", "ggalt","IRkernel")
 install.packages(install_packages)
 
 
--- projects/rnamod/install_packages_R.R
+++ projects/rnamod/install_packages_R.R
@@ -1,3 +1,3 @@
-install_packages <- c("BiocManager", "devtools", "dplyr", "ggplot2", "circlize", "gridExtra", "ggtern", "scatterplot3d","statmod", "ggalt","IRkernel")
+install_packages <- c("BiocManager", "devtools", "dplyr", "languageserver","ggplot2", "circlize", "gridExtra", "ggtern", "scatterplot3d","statmod", "ggalt","IRkernel")
 install.packages(install_packages)
 
