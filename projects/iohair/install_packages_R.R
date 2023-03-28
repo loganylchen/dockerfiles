c665b42cba4af1c141ec304c09bb36bc4dd1d280
loganylchen <yuelong.chen.btr@gmail.com>
2023-03-29 01:01:52 +0800

    1.1.3

--- projects/iohair/install_packages_R.R
+++ projects/iohair/install_packages_R.R
@@ -5,7 +5,7 @@ install.packages(install_packages)
 devtools::install_github("yanlinlin82/ggvenn")
 devtools::install_github("jokergoo/ComplexHeatmap")
 devtools::install_github("YuLab-SMU/clusterProfiler")
-devtools::install_github("GangLiLab/genekitr")
+devtools::install_github("GangLiLab/genekitr@v1.1.3")
 
 BiocManager::install(c("edgeR", "DESeq2", "limma","EnhancedVolcano","PCAtools","MAST"))
 
