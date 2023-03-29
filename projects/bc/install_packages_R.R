94f91d42dc83f472528e1d77a0b9df48d4dd620f
loganylchen <yuelong.chen.btr@gmail.com>
2023-03-29 11:07:25 +0800

    devtools::install_github("kassambara/survminer", build_vignettes = FALSE)

--- projects/bc/install_packages_R.R
+++ projects/bc/install_packages_R.R
@@ -7,7 +7,7 @@ devtools::install_github("jokergoo/ComplexHeatmap")
 devtools::install_github("neurorestore/Libra")
 devtools::install_github("YuLab-SMU/clusterProfiler")
 devtools::install_github("immunogenomics/presto")
-
+devtools::install_github("kassambara/survminer", build_vignettes = FALSE)
 BiocManager::install(c("edgeR", "DESeq2", "limma","EnhancedVolcano","PCAtools","MAST"))
 
 IRkernel::installspec(name = 'BC_R', displayname = 'R BC',user=FALSE)
