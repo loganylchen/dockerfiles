344e51aa6dbfe96fac28cf538e68f9d7a0cf5074
loganylchen <yuelong.chen.btr@gmail.com>
2023-04-02 20:38:50 +0800

    devtools::install_github("GangLiLab/genekitr@v1.1.3")

--- tools/genekitr/install_packages_R.R
+++ tools/genekitr/install_packages_R.R
@@ -1,6 +1,6 @@
-install_packages <- c("BiocManager", "devtools", "dplyr","genekitr","geneset")
+install_packages <- c("BiocManager", "devtools", "dplyr","geneset")
 install.packages(install_packages)
-
+devtools::install_github("GangLiLab/genekitr@v1.1.3")
 
 
 
