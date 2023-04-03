834c6856b6daccb32b4234224a8bebebf451bc00
loganylchen <yuelong.chen.btr@gmail.com>
2023-04-03 08:05:58 +0800

    devtools

--- tools/genekitr/install_packages_R.R
+++ tools/genekitr/install_packages_R.R
@@ -1,8 +1,7 @@
 install_packages <- c("BiocManager", "devtools", "dplyr","geneset")
 install.packages(install_packages)
 
-BiocManager::install("ggtree")
-
+devtools::install_github('YuLab-SMU/ggtree')
 devtools::install_github("YuLab-SMU/clusterProfiler")
 devtools::install_github("GangLiLab/genekitr@v1.1.3")
 
