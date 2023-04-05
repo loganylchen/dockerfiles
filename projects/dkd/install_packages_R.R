1bf49188ab19512c4b953b88514b907a36c3850c
loganylchen <yuelong.chen.btr@gmail.com>
2023-04-05 10:33:47 +0800

    DKD project

--- projects/dkd/install_packages_R.R
+++ projects/dkd/install_packages_R.R
@@ -1,13 +1,18 @@
 install_packages <- c("BiocManager", "devtools","languageserver", "dplyr", "ggplot2", "circlize", "gridExtra", "ggtern", "scatterplot3d", "Seurat", "statmod", "ggalt","IRkernel")
 install.packages(install_packages)
 
+devtools::install_github('YuLab-SMU/ggtree')
+BiocManager::install('clusterProfiler')
+devtools::install_github("GangLiLab/genekitr@v1.1.3")
 
 devtools::install_github("yanlinlin82/ggvenn")
 devtools::install_github("jokergoo/ComplexHeatmap")
 devtools::install_github("neurorestore/Libra")
-devtools::install_github("YuLab-SMU/clusterProfiler")
 devtools::install_github("immunogenomics/presto")
 
 BiocManager::install(c("edgeR", "DESeq2", "limma","EnhancedVolcano","PCAtools","MAST"))
 
 IRkernel::installspec(name = 'DKD_R', displayname = 'R DKD',user=FALSE)
+
+
+
