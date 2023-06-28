cf9cf57ad958a77ee9e2527c854d99d28d84e67a
Logan <yuelong.chen@link.cuhk.edu.hk>
2023-06-28 17:08:39 +0800

    :ambulance: install cowplot

--- tools/genekitr/install_packages_R.R
+++ tools/genekitr/install_packages_R.R
@@ -1,4 +1,4 @@
-install_packages <- c("BiocManager", "devtools", "dplyr","geneset",'ggplot2','ggfun')
+install_packages <- c("BiocManager", "devtools", "dplyr","geneset",'ggplot2','ggfun', 'cowplot')
 install.packages(install_packages)
 
 devtools::install_github('YuLab-SMU/ggtree')
