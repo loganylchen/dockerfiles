07cbe106c2604e8a46ae0b4be75fd4c801534d5d
loganylchen <yuelong.chen.btr@gmail.com>
2023-07-12 11:37:45 +0800

    install.extras('NMF')

--- tools/bayesprism/install_packages_R.R
+++ tools/bayesprism/install_packages_R.R
@@ -2,7 +2,7 @@ install_packages <- c("snowfall", "NMF")
 bioc_packages <- c("BiocParallel","scran")
 install.packages(install_packages)
 BiocManager::install(bioc_packages)
-
+install.extras('NMF')
 devtools::install_github("Danko-Lab/BayesPrism/BayesPrism")
 
 
