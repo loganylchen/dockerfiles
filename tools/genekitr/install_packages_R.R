cd6ef926c8c97113de2f154204c7251f11b30ab7
loganylchen <yuelong.chen.btr@gmail.com>
2023-08-04 12:47:20 +0800

    update basic image to btrspg/vscode-base:0.0.3

--- tools/genekitr/install_packages_R.R
+++ tools/genekitr/install_packages_R.R
@@ -1,8 +1,10 @@
 install_packages <- c("BiocManager", "devtools", "dplyr","geneset",'ggplot2','ggfun', 'cowplot')
 install.packages(install_packages)
-
+print('Install YuLab-SMU/ggtree')
 devtools::install_github('YuLab-SMU/ggtree')
+print('Install clusterProfiler')
 BiocManager::install('clusterProfiler')
+print('Install GangLiLab/genekitr')
 devtools::install_github("GangLiLab/genekitr")
 
 
