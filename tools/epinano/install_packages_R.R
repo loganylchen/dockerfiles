9b694a72658594c08f9a795a55daaa25b9df6c48
Yuelong Chen <yuelong.chen.btr@gmail.com>
2024-03-28 12:01:37 +0800

    eligos2

---
+++ tools/epinano/install_packages_R.R
@@ -0,0 +1,2 @@
+install.packages(c("forcats",'optparse','stringr','dplyr','purrr','readr','tidyr','tibble','tidyverse','ggrepel','car','ggplot2','reshape2','outliers'), repos='http://cran.us.r-project.org', dependencies=TRUE)
+
