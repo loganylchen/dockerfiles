41a5e8c2a4461e557490db159185990f7d620537
Yuelong Chen <yuelong.chen.btr@gmail.com>
2024-05-23 13:40:34 +0800

    epinano

--- tools/epinano/install_packages_R.R
+++ tools/epinano/install_packages_R.R
@@ -1,2 +1,4 @@
+
+install.packges("https://cran.r-project.org/src/contrib/Archive/Matrix/Matrix_1.6-0.tar.gz", repos=NULL, type="source")
 install.packages(c("forcats",'optparse','stringr','dplyr','purrr','readr','tidyr','tibble','tidyverse','ggrepel','car','ggplot2','reshape2','outliers'), repos='http://cran.us.r-project.org', dependencies=TRUE)
 
